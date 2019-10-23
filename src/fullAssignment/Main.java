f61ae8eb3f5ced5a3b29327f2fca5a486fea00db
Conor Baird <conor.baird@my.jcu.edu.au>
2019-10-23 15:00:00 +1000

    Implemented a rudimentary menubar system currently not being used however and will be utilised later in developement

--- src/fullAssignment/Main.java
+++ src/fullAssignment/Main.java
@@ -2,13 +2,32 @@ package fullAssignment;
 
 import fullAssignment.Model.Roads;
 import fullAssignment.View.MainFrame;
-import view.NumberedButton;
+
 
 import javax.swing.*;
 
 public class Main {
+    private static int time = 0;
+
     public static void main(String[] args){
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame("Traffic Simulator");
+        JMenuBar menuBar = new JMenuBar();
+        frame.setJMenuBar(menuBar);
+        JMenu file = new JMenu("File");
+        JMenu start = new JMenu("Start");
+        JMenu stop = new JMenu("Stop");
+        JMenuItem Load = new JMenuItem("Load");
+        JMenuItem Save = new JMenuItem("Save");
+        JMenu currentTime = new JMenu(String.valueOf(time));
+        file.add(Load);
+        file.add(Save);
+        menuBar.add(file);
+        menuBar.add(start);
+        menuBar.add(stop);
+        menuBar.add(currentTime);
+
+
+
         MainFrame mainFrame = new MainFrame();
         frame.add(mainFrame);
         frame.pack();
